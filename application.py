b031876024df023f00b4ca89a998a0d5a79fd47b
owenkiprop <kipzowen@gmail.com>
2021-11-10 18:25:09 +0300

    Added registration page

--- application.py
+++ application.py
@@ -4,5 +4,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def layout():
+def homepage():
     return render_template("home.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
