060d66c2ffd2e804ea03f24ed491964dbf6813d8
owenkiprop <kipzowen@gmail.com>
2021-11-21 01:00:56 +0300

    Retrieves and saves API data to database

--- helpers.py
+++ helpers.py
@@ -1,6 +1,10 @@
 from functools import wraps
-
+import requests
+import os
+from cs50 import SQL
 from flask import session, redirect
+
+db = SQL('sqlite:///database.db')
 
 def login_required(f):
     
@@ -11,3 +15,111 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+def convert(currency_from, currency_to):
+
+    url = "https://alpha-vantage.p.rapidapi.com/query"
+    querystring = {"to_currency":currency_to,"function":"CURRENCY_EXCHANGE_RATE","from_currency":currency_from}
+
+    headers = {
+        'x-rapidapi-host': "alpha-vantage.p.rapidapi.com",
+        'x-rapidapi-key': os.getenv("API_KEY")
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    sql_string = "SELECT id, name FROM currencies WHERE name=? or name=?"
+    rows = db.execute(sql_string, currency_to, currency_from)
+    if rows[0]["name"] == currency_from:
+        id_from = rows[0]["id"]
+        id_to = rows[1]["id"]
+    else:
+        id_from = rows[1]["id"]
+        id_to = rows[0]["id"]
+
+    sql_insert = "INSERT INTO conversions VALUES (?, ?, ? )"
+    db.execute(sql_insert, id_from, id_to, response.text)
+
+
+def monthly_trend(currency_from, currency_to):      # relative to the KES
+    url = "https://alpha-vantage.p.rapidapi.com/query"
+    querystring = {"from_symbol":currency_from,"to_symbol":currency_to,"function":"FX_MONTHLY","datatype":"json"}
+
+    headers = {
+        'x-rapidapi-host': "alpha-vantage.p.rapidapi.com",
+        'x-rapidapi-key': os.getenv("API_KEY")
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    sql_string = "SELECT id, name FROM currencies WHERE name=? or name=?"
+    rows = db.execute(sql_string, currency_to, currency_from)
+    if rows[0]["name"] == currency_from:
+        id_from = rows[0]["id"]
+        id_to = rows[1]["id"]
+    else:
+        id_from = rows[1]["id"]
+        id_to = rows[0]["id"]
+
+    sql_insert = "INSERT INTO monthly_trends VALUES (?, ?, ? )"
+    print(response.text)
+    db.execute(sql_insert, id_from, id_to, response.text)
+
+
+
+def daily_trend(currency_from, currency_to):      # relative to the KES
+    url = "https://alpha-vantage.p.rapidapi.com/query"
+    querystring = {"from_symbol":currency_from,"function":"FX_DAILY","to_symbol":currency_to,"outputsize":"compact","datatype":"json"}
+
+    headers = {
+        'x-rapidapi-host': "alpha-vantage.p.rapidapi.com",
+        'x-rapidapi-key': os.getenv("API_KEY")
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    sql_string = "SELECT id, name FROM currencies WHERE name=? or name=?"
+    rows = db.execute(sql_string, currency_to, currency_from)
+    if rows[0]["name"] == currency_from:
+        id_from = rows[0]["id"]
+        id_to = rows[1]["id"]
+    else:
+        id_from = rows[1]["id"]
+        id_to = rows[0]["id"]
+
+    sql_insert = "INSERT INTO daily_trends VALUES (?, ?, ? )"
+    print(response.text)
+    db.execute(sql_insert, id_from, id_to, response.text)
+
+def get_currency_names():
+    sql_string = "SELECT name FROM currencies"
+    rows = db.execute(sql_string)
+
+    global currency_list
+    currency_list = []
+    for i in range(0, len(rows)):
+        currency_list.append(rows[i]["name"])
+    print(currency_list)
+
+def get_daily_standings():
+    for i in range(0, len(currency_list)):
+        for j in range(0, len(currency_list)):
+            if i == j:
+                continue
+            daily_trend(currency_list[i], currency_list[j])
+
+def get_conversions():
+    for i in range(0, len(currency_list)):
+        for j in range(0, len(currency_list)):
+            if i == j:
+                continue
+            convert(currency_list[i], currency_list[j])
+
+def get_monthly_standings():
+    for i in range(0, len(currency_list)):
+        for j in range(0, len(currency_list)):
+            if i == j:
+                continue
+            monthly_trend(currency_list[i], currency_list[j])
+
+
+get_currency_names()
+get_conversions()
+get_daily_standings()
+get_monthly_standings()
--- static/database.sql
+++ static/database.sql
@@ -1,17 +1,6 @@
-CREATE DATABASE db50;
-CREATE TABLE users(
-    id SMALLINT PRIMARY KEY AUTO_INCREMENT,
-    username CHAR(20) NOT NULL UNIQUE, 
-    password INT NOT NULL,
-    email CHAR(25) NOT NULL,
-    date DATE DEFAULT GETDATE()
-);
-CREATE TABLE currencies(
-    id TINYINT PRIMARY KEY AUTO_INCREMENT,
-    name CHAR(10),
-    description CHAR(30)
-);
-CREATE TABLE trades(
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE trades
+(
     user1 SMALLINT,
     currency_user1 TINYINT,
     value_user1 INT,
@@ -23,18 +12,57 @@ CREATE TABLE trades(
     FOREIGN KEY(currency_user1) REFERENCES currencies(id),
     FOREIGN KEY(currency_user1) REFERENCES currencies(id)
 );
-CREATE TABLE ownerships(
+CREATE TABLE ownerships
+(
     userid SMALLINT,
-    currency_id TINYINT,
+    currency_id TINYINT, value int,
     FOREIGN KEY(userid) REFERENCES users(id),
     FOREIGN KEY(currency_id) REFERENCES currencies(id)
 );
-CREATE TABLE offers(
+CREATE TABLE offers
+(
     userid SMALLINT,
     currency TINYINT,
     currency_needed TINYINT,
     FOREIGN KEY(userid) REFERENCES users(id),
     FOREIGN KEY(currency) REFERENCES currencies(id),
     FOREIGN KEY(currency_needed) REFERENCES currencies(id)
-)
-
+);
+CREATE TABLE users
+(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username CHAR(20) NOT NULL,
+email VARCHAR(25) NOT NULL,
+password INT NOT NULL,
+date DATE DEFAULT (DATETIME())
+);
+CREATE TABLE currencies
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name CHAR(10),
+    description CHAR(20) 
+);
+CREATE TABLE conversions
+(
+    currency_from_id INT,
+    currency_to_id INT,
+    trend TEXT,
+    FOREIGN KEY(currency_from_id) REFERENCES currencies(id),
+    FOREIGN KEY(currency_to_id) REFERENCES currencies(id) 
+);
+CREATE TABLE monthly_trends
+(
+    currency_from_id INT,
+    currency_to_id INT,
+    trend TEXT,
+    FOREIGN KEY(currency_from_id) REFERENCES currencies(id),
+    FOREIGN KEY(currency_to_id) REFERENCES currencies(id) 
+);
+CREATE TABLE daily_trends
+(
+    currency_from_id INT,
+    currency_to_id INT,
+    trend TEXT,
+    FOREIGN KEY(currency_from_id) REFERENCES currencies(id),
+    FOREIGN KEY(currency_to_id) REFERENCES currencies(id) 
+);
